26d2e58c5b3ccbcef215e45d3b50f37df5864a43
Doris Morales <moralesdoris880@gmail.com>
2022-03-30 16:40:56 -0400

    Completed assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordInvalid, with: :r_unprocessable_entity
+    skip_before_action :authorized, only: :create
+
+    def show 
+        current_user = User.find_by(id:session[:user_id])
+        if current_user
+            render json: current_user
+        else
+            render json: {error: "Not Authorized"}, status: :unauthorized
+        end
+    end
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+          r_unprocessable_entity
+        end
+    end
+
+    private
+
+    def r_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+    def user_params
+        params.permit(:username,:password,:password_confirmation)
+    end 
+
+    
+end
